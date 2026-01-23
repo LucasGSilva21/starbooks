024d16b6b7f7c7dcfaf34f7d90e61786106cd11b
Lucas Gabriel <lucasgsilva2102@gmail.com>
2026-01-23 13:16:05 -0300

    chore: project setup

---
+++ src/test/java/com/github/lucasgsilva21/starbooks/StarbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasgsilva21.starbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
